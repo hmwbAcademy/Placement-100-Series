814b90681c2d718565edefed0b2dfff896023c7b
HMWB Academy <hmwbacademy@gmail.com>
2023-12-17 18:51:45 +0530

    6/100 problem code.

---
+++ #6-product-of-array-except-self.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [0] * len(nums)
+        left = [0] * len(nums)
+        right = [0] * len(nums)
+
+        left[0] = 1
+        for i in range(1, len(nums)):
+            left[i] = left[i-1] * nums[i-1]
+
+        right[-1] = 1
+        for i in range(len(nums)-2, -1, -1):
+            right[i] = right[i+1] * nums[i+1]
+
+        for i in range(0, len(nums)):
+            answer[i] = left[i] * right[i]
+
+        return answer
